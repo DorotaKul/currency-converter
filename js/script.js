575af7cff9e5f1dec588d216be3b841a0677218b
Dorota Kul <kuldorota1@gmail.com>
2023-10-31 11:56:30 +0100

    changed let to const

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-let amountElement= document.querySelector (".js-amount");
-let currencyElement= document.querySelector (".js-currency");
-let resultElement= document.querySelector (".js-result");
-let formElement= document.querySelector (".js-form");
+const amountElement= document.querySelector (".js-amount");
+const currencyElement= document.querySelector (".js-currency");
+const resultElement= document.querySelector (".js-result");
+const formElement= document.querySelector (".js-form");
 
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-let amount = +amountElement.value;
-let currency = currencyElement.value;
+const amount = +amountElement.value;
+const currency = currencyElement.value;
 
-let rateUSD = 4.35;
-let rateGBP = 5.30;
-let rateEUR = 4.61;
+const rateUSD = 4.35;
+const rateGBP = 5.30;
+const rateEUR = 4.61;
 
 switch (currency){
     case "USD":
